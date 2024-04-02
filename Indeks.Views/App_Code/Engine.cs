f108fa66268c21285a1881b9169f4d86a43306cb
User <User@DESKTOP-IKONQE5>
2024-04-02 13:56:51 +0300

    .

---
+++ Indeks.Views/App_Code/Engine.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Web;
+using System.Xml.Linq;
+using System.Windows.Forms;
+
+//using Han.Xml;
+using Indeks.Views.Models;
+using Indeks.Data.Base;
+using System.Reflection;
+namespace Indeks.Views
+{
+  public class Engine
+  {
+        private static string XmlPath = "";//Path.Combine(Application.StartupPath, "Xmls\\settings.xml");
+        private static string PPath = "";
+    static Engine()
+    {
+           
+
+#if (DEBUG)
+            string path = Application.StartupPath;
+            string newPath = Path.GetFullPath(Path.Combine(path, @"..\..\"));
+            PPath = Path.Combine(newPath,"db");
+            XmlPath = Path.Combine(PPath, "Xmls\\settings.xml");
+
+#else
+        
+            PPath = Path.Combine(Application.StartupPath,"db"); 
+            XmlPath = Path.Combine(PPath, "Xmls\\settings.xml");
+           
+#endif
+
+        }
+        public static string RaporPath() {
+
+      //return @"D:\Project2010\Han\Indeks.Views\Rapor";
+      return Path.Combine(PPath, "Rapor"); ;
+    }
+    public static string DokumanPath() {
+
+        //return @"D:\Project2010\Han\Indeks.Views\Dokumanlar";
+       return Path.Combine(PPath, "Dokumanlar"); ;
+    }
+    public static string DataBasePath() {
+  
+       //return @"D:\Project2010\Han\Indeks.Views\Db";
+        return Path.Combine(PPath, "Db");
+    }
+    public static string SqlLitePath() {
+
+        //return @"D:\Project2010\Han\Indeks.Views\Db";
+        return Path.Combine(PPath, "SqlLite");
+    }
+    public static string GetSqlLiteConString() {
+        string veriTaban="Data Source=";       
+        veriTaban+=Path.Combine( DataBasePath(),"Indeks.db");
+        veriTaban+=";Version=3;";
+        //LogWrite.Write(new Exception(veriTaban));
+        return veriTaban;
+    }
+    public static void SaveSettings(Settings settings) {
+        XDocument xmlDoc;
+        xmlDoc = XDocument.Load(XmlPath);
+        var x = (from s in xmlDoc.Descendants("Settings")
+                 select
+                  s).Single();
+        x.Element("DataBase").Value = settings.DataBase.ToStringOrEmpty();
+        x.Element("ConnectionString").Value = settings.ConnectionString;
+        x.Element("Paket").Value = settings.Paket.ToString();
+        x.Element("Kurulum").Value = settings.Kurulum;  
+        xmlDoc.Save(XmlPath);
+    
+    }
+    public static string GetConString()
+    {     
+        Settings set = FindSettings();
+        if (set.DataBase == "SqlLite")
+            return GetSqlLiteConString();
+        else
+            return set.ConnectionString;
+    }
+    public static Settings FindSettings() {
+        XDocument xmlDoc = XDocument.Load(XmlPath);
+        var query = from x in xmlDoc.Descendants("Settings")
+                    select new Settings
+                    {
+                        ConnectionString = x.Element("ConnectionString").Value,
+                        DataBase = x.Element("DataBase").Value,
+                        Paket = GetIndeksPaket(x.Element("Paket").Value),
+                        Kurulum = x.Element("Kurulum").Value
+                    };
+
+        return query.FirstOrDefault();//.SingleOrDefault();
+            //return new Settings(); // fatura form hata düzeliyor aktif edince
+
+    }
+    public static SqlServerType GetSqlServerType() {
+        string sql = FindSettings().DataBase;
+        if (sql == "MsSql")
+            return SqlServerType.SQL2005;
+        else
+            return SqlServerType.SqlLite;
+    }
+    private static IndeksPaket GetIndeksPaket(string value) {
+        if (string.IsNullOrEmpty(value))
+            return IndeksPaket.Eko;
+        return (IndeksPaket)Enum.Parse(typeof(IndeksPaket), value);
+    }
+    public static string Versiyon() {
+        try {
+            System.Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            return version.Major + "." + version.Minor + "." + version.Build ;
+        } catch (Exception) {
+            return "?.?.?";
+        }
+    }
+   
+   
+  }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# IndeksTicari
+Açık kaynak kodlu ön muhasebe programı
+### [İndir](/indeksSetup.zip?raw=true) 
+### programın çalışması için netframework kurulması gereklidir,eğer kurulu değilse,kurmak için [tıklayın](https://www.microsoft.com/en-us/download/details.aspx?id=24872)
+##### Kullanıcı adı:indeks  şifre:123
+##### YardımcıProgramlar -> Kullanıcı menusunden kullanıcı şifresini değiştirebilirsiniz
+
+
